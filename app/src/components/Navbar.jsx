1b701498998dc6c84fedf005031a94351649ec00
milliorn <scottmilliorn@gmail.com>
2022-09-11 12:33:04 -0700

    SignIn

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import SignIn from "./SignIn";
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -13,6 +14,7 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>chat app</h1>
+      <SignIn />
     </div>
   );
 };
