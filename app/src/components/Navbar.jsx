84ea7dec56f5903778f2568b861207afef4ebff7
milliorn <scottmilliorn@gmail.com>
2022-09-11 12:42:37 -0700

    LogOut

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import SignIn from "./SignIn";
+import LogOut from "./LogOut";
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -14,7 +15,8 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>chat app</h1>
-      <SignIn />
+      {/*Display sign in or sign out based on state */}
+      {user ? <LogOut /> : <SignIn />}
     </div>
   );
 };
