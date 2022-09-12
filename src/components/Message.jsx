7782c83e314237af0bdaacffe66d841f98230e48
milliorn <scottmilliorn@gmail.com>
2022-09-11 23:39:55 -0700

    Chat, Message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,9 +3,9 @@ import { auth } from "../firebase";
 
 const style = {
   message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  name: `absolute -mt-16 text-gray-600 text-xs`,
+  received: `bg-gray-200 text-black float-left rounded-br-full`,
+  sent: `bg-blue-600 text-white flex-row-reverse text-end float-right rounded-bl-full`,
 };
 
 const Message = ({ message }) => {
